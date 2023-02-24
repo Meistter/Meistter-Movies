521a0733058d1d3c4249660597f317306b5a3c52
Meistter <josueperez723@gmail.com>
2023-02-24 16:00:57 -0400

    d

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -17,13 +17,8 @@ export class PeliculasService {
   }
   getMoviesMainPaginated(){
 
-    const { scrollTop, scrollHeight, clientHeight} = document.documentElement
-    const pageIsNotMax = this.page < this.maxPage; //esta variable la usamos para verificar que no lleguemos a la ultima pagina existente en la api, para no cargar mas de las paginas existentes en la api lo cual daria error
-
-    // if ((( scrollTop + clientHeight) >= (scrollHeight - 15)) && pageIsNotMax){ //esta es la validacion para saber si la persona llego al final del scroll de la pagina, la comparacion de estos valores nos da como resultado si esta o no al final, cada dato representa un numero
 
     this.page++;
-    console.log(this.page);
 
     return this.http.get<Datos>(this.API, {params: {page: this.page}})
 
