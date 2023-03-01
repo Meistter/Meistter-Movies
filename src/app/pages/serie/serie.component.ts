622ae14850100be335a1ee7d2bdb37a6f9b1a3e3
Meistter <josueperez723@gmail.com>
2023-03-01 16:05:28 -0400

    movie and serie detail

--- src/app/pages/serie/serie.component.ts
+++ src/app/pages/serie/serie.component.ts
@@ -11,22 +11,8 @@ import { environment } from 'src/enviroments/enviroments';
 export class SerieComponent {
   constructor(private route: ActivatedRoute, private seriesService: SeriesService){}
   imgurl = environment.IMG_BASE
-  serie: Movie = {
-  adult: false,
-  backdrop_path: 'string',
-  genre_ids: [],
-  id: 0,
-  media_type: 'string',
-  original_languge: 'string',
-  original_title: 'string',
-  overview: 'string',
-  popularity: 0,
-  poster_path: 'string',
-  release_date: 'string',
-  title: 'string',
-  video: false,
-  vote_average: 0,
-  vote_count: 0
+  serie: any = { //modificar tipado para obtener más cosas
+
   }
   serieId: string | null = null
   ngOnInit(): void {
