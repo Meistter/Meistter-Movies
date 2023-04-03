5c38c8acb4c16a200de425faaaaf00fec2ccb7d8
Meistter <josueperez723@gmail.com>
2023-04-03 10:44:08 -0400

    login

--- src/app/website/pages/login/login.component.ts
+++ src/app/website/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MyValidators } from 'src/app/utils/validators';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  hide = true //esto es para ocultar la contraseña
 
+  form = new FormGroup({
+    email : new FormControl('',[Validators.required, Validators.email]), //de esta forma podemos declarar los FormControl de forma más ordenada, dentro de un FormGroup
+    password : new FormControl('',[Validators.required, Validators.minLength(5)]),
+    })
+
+
+    getRequiredErrorMessage(){
+      return 'Este campo es requerido'
+    }
+    getEmailErrorMessage(){
+      return 'Formato Invalido'
+    }
+    onSubmit(){
+      console.log('form: ',this.form.value);
+    }
 }
