d6332e725c5b4b527ebf891866a0eea23dd030d4
Josue Perez <josueperez723@gmail.com>
2023-04-15 15:29:26 -0400

    responsive

--- src/app/website/pages/movie/movie.component.ts
+++ src/app/website/pages/movie/movie.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Categoria } from 'src/app/models/caterogias';
 import { Datos, Movie } from 'src/app/models/movie';
 import { PeliculasService } from 'src/app/services/peliculas.service';
 import { environment } from 'src/enviroments/enviroments';
@@ -15,7 +16,7 @@ export class MovieComponent implements OnInit{
 
   pelicula: any = { //modificar tipado
   }
-  generos: any = []
+  generos: Categoria[] = [{id: 0, name: ''}]
 
   datos: Datos = {
     page: 0,
@@ -23,17 +24,20 @@ export class MovieComponent implements OnInit{
     total_pages: 0,
     total_results: 0,
   };
-  peliculas: Movie[] = []
+  peliculas: Movie[] = [
+    {adult: false,backdrop_path: 'string',genres: [],id: -1,media_type: 'string',original_languge: 'string',original_title: 'string',overview: 'string',popularity: 0,poster_path: 'string',release_date: 'string',title: 'string',video: false,vote_average: 0,vote_count: 0}
+  ]
 
   movieId: string | null = null
   ngOnInit(): void {
     this.route.paramMap.subscribe(params=>{
       this.movieId = params.get('id')
-      console.log(this.movieId);
+
 
       //ahora consultamos a la API
       if(this.movieId){
-      this.peliculasService.getMovie(this.movieId).subscribe(data=>{this.pelicula = data, this.generos = data.genres
+      this.peliculasService.getMovie(this.movieId).subscribe(data=>{this.pelicula = data, this.generos = data.genres, console.log(this.generos);
+
       })
       }
       //peliculas relacionadas
--- src/app/website/pages/seriep/seriep.component.ts
+++ src/app/website/pages/seriep/seriep.component.ts
@@ -20,7 +20,9 @@ export class SeriepComponent {
     total_pages: 0,
     total_results: 0,
   };
-  series: Movie[] = []
+  series: Movie[] = [
+    {adult: false,backdrop_path: 'string',genres: [],id: -1,media_type: 'string',original_languge: 'string',original_title: 'string',overview: 'string',popularity: 0,poster_path: 'string',release_date: 'string',title: 'string',video: false,vote_average: 0,vote_count: 0}
+  ]
   generos: any = []
   serieId: string | null = null
   ngOnInit(): void {
