1231e091f3ebedb8dbd72680a0359c20b2014abe
Meistter <josueperez723@gmail.com>
2023-06-02 12:03:48 -0400

    avances estilos CSS home y movie

--- src/app/website/pages/home/home.component.ts
+++ src/app/website/pages/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import { PeliculasService } from 'src/app/services/peliculas.service';
+import { Movie } from 'src/app/models/movie';
+import { environment } from 'src/enviroments/enviroments';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  constructor(private peliculasService:PeliculasService){    
+  }
+  movieContent : Movie[] = []
+  headerBackground = ''
+  min = Math.ceil(1);
+  max = Math.floor(20);
+  numero = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
+  ngOnInit(): void {
+   this.peliculasService.getTrendingMovies().subscribe((data)=>{this.movieContent = data.results, this.headerBackground = environment.IMG_BASE_ORIGINAL + this.movieContent[this.numero].backdrop_path;
+   })
+   
+  }
+  
+  
 }
--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: false,
   API : 'https://api.themoviedb.org/3',
   API_KEY : '1dd6a9bf71ecd351e5f9ea03b82ff229',
-  IMG_BASE : 'https://image.tmdb.org/t/p/w300',
+  IMG_BASE : 'https://image.tmdb.org/t/p/w400',
+  IMG_BASE_ORIGINAL : 'https://image.tmdb.org/t/p/original',
   firebase: {
     apiKey: "AIzaSyD9B_GHfazxmy9B4bcZbFFLH9WQs0dnOFk",
     authDomain: "meistter-movies.firebaseapp.com",
