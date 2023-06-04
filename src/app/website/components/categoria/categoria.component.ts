01f68c72d377b17974b1d5011fa1eaa352f4352f
Josue Perez <josueperez723@gmail.com>
2023-06-03 22:05:31 -0400

    todo el landing listo

--- src/app/website/components/categoria/categoria.component.ts
+++ src/app/website/components/categoria/categoria.component.ts
@@ -1,20 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { Categoria } from 'src/app/models/caterogias';
+import { environment } from 'src/enviroments/enviroments';
 @Component({
   selector: 'app-categoria',
   templateUrl: './categoria.component.html',
   styleUrls: ['./categoria.component.css']
 })
 export class CategoriaComponent {
-
+  URL = environment.IMG_BASE
   @Input() categoria: Categoria ={
     id: 0,
     name: ''
   }
-  @Input() backgroundImage = []
+  @Input() bgimage = ''
   
   ngOnInit(): void{
   
+    console.log('esto es en categoria',this.bgimage);
+   
+    
     
   }
 }
--- src/app/website/components/categorias/categorias.component.ts
+++ src/app/website/components/categorias/categorias.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Categoria, CatObj } from 'src/app/models/caterogias';
+import { Movie } from 'src/app/models/movie';
 import { CategoriasService } from 'src/app/services/categorias.service';
 
 @Component({
@@ -14,10 +15,28 @@ export class CategoriasComponent {
   genres: Categoria[] = [
     {id:-1,name:''}, {id:-1,name:''}, {id:-1,name:''}, {id:-1,name:''}, {id:-1,name:''}, {id:-1,name:''}, {id:-1,name:''}, {id:-1,name:''}
 ]
-  backgroundImage = []
+  backgroundImage : Movie[] = [{adult: false,
+    backdrop_path: '',
+    genres: [],
+    id: 0,
+    media_type: '',
+    original_languge: '',
+    original_title: '',
+    overview: '',
+    popularity: 0,
+    poster_path: '',
+    release_date: '',
+    title: '',
+    video: false,
+    vote_average: 0,
+    vote_count: 0
+    } //estado inicial
+  ]
+  
   ngOnInit(): void{
-    this.categoriasService.getCategories().subscribe(data=>{this.genres=data.genres})
+    this.categoriasService.getCategories().subscribe(data=>{this.genres=data.genres, console.log('en categorias',this.genres);
+    })
     // Buscamos imagenes para llenar nuestro array y mostrar en caterogias
-    // this.categoriasService.getCategoryById('12',1).subscribe(data=>{this.backgroundImage = data.results, console.log(this.backgroundImage)})
+    this.categoriasService.getCategoryById('12',1).subscribe(data=>{this.backgroundImage = data.results})
   }
 }
