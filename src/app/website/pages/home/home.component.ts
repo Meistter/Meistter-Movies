e47c71f32505ff6f431f208d2d3d3828676d9bd4
Josue Perez <josueperez723@gmail.com>
2023-06-05 20:51:13 -0400

    paginas funcionando y favoritos

--- src/app/website/pages/home/home.component.ts
+++ src/app/website/pages/home/home.component.ts
@@ -14,8 +14,8 @@ export class HomeComponent {
   headerBackground = ''
   movieTitle = ''
   movieId = 0
-  min = Math.ceil(1);
-  max = Math.floor(20);
+  min = Math.ceil(0);
+  max = Math.floor(19);
   numero = Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   ngOnInit(): void {
    this.peliculasService.getTrendingMovies().subscribe((data)=>{this.movieContent = data.results, this.headerBackground = environment.IMG_BASE_ORIGINAL + this.movieContent[this.numero].backdrop_path, this.movieTitle = this.movieContent[this.numero].title, this.movieId = this.movieContent[this.numero].id})
